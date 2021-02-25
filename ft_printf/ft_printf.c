0ef2c7fbcee6b2d7c5921d1a76ec5a6831c53d1c
jhpark <chicjhpark@gmail.com>
2021-02-26 00:47:52 +0900

    di mod

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -28,6 +28,7 @@ void    init_tag(f_tag *tag)
     tag->prec = 0;
     tag->size = 0;
     tag->dot = 0;
+    tag->nbr = 0;
     tag->lspace = 0;
     tag->rspace = 0;
 }
@@ -118,7 +119,14 @@ void    sort_width_prec(f_tag *tag)
         if (tag->dot == 1)
             tag->prec = va_arg(tag->ap, int);
         else
+        {
             tag->width = va_arg(tag->ap, int);
+            if (tag->width < 0)
+            {
+                tag->minus = 1;
+                tag->width *= -1;
+            }
+        }
     }
     else if (ft_isdigit(*tag->fmt) != 0)
     {
@@ -127,42 +135,32 @@ void    sort_width_prec(f_tag *tag)
         else
             tag->width = (tag->width * 10) + *tag->fmt - '0';
     }
-	if (tag->width < 0)
-	{
-		tag->width *= -1;
-		if (tag->minus == 0 && tag->zero == 0)
-			tag->minus = 1;
-	}
 }
 
 void    print_di(f_tag *tag)
 {
-    digits_size(tag);
     if (tag->nbr < 0)
     {
         tag->lspace -= 1;
         tag->rspace -= 1;
-        tag->zero -= 1;
     }
-    while (tag->lspace-- > 0)
-        tag->res += ft_putchar(' ');
-	if (tag->nbr == 0 && tag->dot == 1 && tag->prec == 0)
-	{
-		if (tag->width > 0)
-			while (tag->rspace-- > 0 || tag->nbr-- >= 0)
-				tag->res += ft_putchar(' ');
-		return ;
-	}
+    if (tag->zero != 1)
+        while (tag->lspace-- > 0)
+            tag->res += ft_putchar(' ');
+    else
+        while (tag->lspace-- > 0)
+            tag->res += ft_putchar('0');
+    if (tag->nbr == 0 && tag->dot == 1)
+    {
+        while (tag->rspace-- > 0)
+            tag->res += ft_putchar(' ');
+        return ;
+    }
     if (tag->nbr < 0)
         tag->res += ft_putchar('-');
-    while (tag->zero-- > 0 && tag->prec <= 0)
-        tag->res += ft_putchar('0');
     while (tag->prec-- > 0)
         tag->res += ft_putchar('0');
-    if (tag->nbr == 0)
-        tag->res += ft_putchar('0');
-    else
-        tag->res += ft_putstr(ft_itoa_sign(tag));
+    tag->res += ft_putstr(ft_itoa_sign(tag));
     while (tag->rspace-- > 0)
         tag->res += ft_putchar(' ');
 }
@@ -177,56 +175,34 @@ void    check_tag(f_tag *tag)
 {
     if (*tag->fmt == '-')
         tag->minus = 1;
-    else if (*tag->fmt == '0' && tag->width == 0 && tag->dot == 0)
+    else if (*tag->fmt == '0' && tag->width == 0)
         tag->zero = 1;
-    else if (*tag->fmt == '*' || *tag->fmt == '.' || ft_isdigit(*tag->fmt) != 0)
+    else if (*tag->fmt == '*' || *tag->fmt == '.' || ft_isdigit(*tag->fmt))
         sort_width_prec(tag);
-    else if (tag->zero != 0 && tag->minus == 1)
-        tag->zero = 0;
 }
 
 void    sort_tag(f_tag *tag)
 {
-    if (tag->prec >= tag->width && tag->prec >= tag->size)
-    {
-        tag->prec -= tag->size;
-        tag->zero = 0;
-    }
-    else if (tag->width > tag->prec && tag->width > tag->size)
+    if (tag->minus == 1 || tag->dot == 1)
+        tag->zero == 0;
+    if (tag->width > tag->prec && tag->width > tag->size)
     {
         if (tag->prec > tag->size)
         {
             if (tag->minus == 1)
                 tag->rspace = tag->width - tag->prec;
-			else
-				tag->lspace = tag->width - tag->prec;
-		    tag->zero = 0;
-            tag->prec -= tag->size;
+            else
+                tag->lspace = tag->width - tag->prec;
         }
         else
         {
             if (tag->minus == 1)
                 tag->rspace = tag->width - tag->size;
-			else
-			{
-				if (tag->zero == 1 && tag->prec <= 0)
-                	tag->zero = tag->width - tag->size;
-            	else
-                	tag->lspace = tag->width - tag->size;
-			}
-			if (tag->prec > 0)
-			{
-            	tag->prec = 0;
-				tag->zero = 0;
-			}
+            else
+                tag->lspace = tag->width - tag->size;
         }
     }
-    else
-    {
-		if (tag->prec > 0)
-        	tag->prec = 0;
-        tag->zero = 0;
-    }
+    tag->prec -= tag->size;
 }
 
 void    check_type(f_tag *tag)
@@ -237,6 +213,7 @@ void    check_type(f_tag *tag)
         digits_size(tag);
     }
 }
+
 void    sort_format(f_tag *tag)
 {
     tag->res = 0;
@@ -273,12 +250,7 @@ int     ft_printf(const char *format, ...)
     return (tag.res);
 }
 
-/*int     main(void)
+int     main(void)
 {
-    ft_printf("imt : %.0d\n", 0);
-    printf("org : %.3d\n", 13862);
-    ft_printf("imt : %08.5d\n", 34);
-    printf("org : %08.5d\n", 34);
-
-    return (0);
-}*/
+    printf("org : |%")
+}
