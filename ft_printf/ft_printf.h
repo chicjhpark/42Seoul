656a76f709595186327788618a3a8dfd1b7bc834
jhpark <chicjhpark@gmail.com>
2021-02-26 08:35:09 +0900

    ft_printf.h mod

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -20,31 +20,31 @@
 
 typedef struct	s_tag
 {
-	va_list	ap;
-	char	*fmt;
-	char	*str;
-	int		minus;
-	int		zero;
-	int		width;
-	int		prec;
-	int		size;
-	int		dot;
-	int		res;
-	int		nbr;
-	int		lspace;
-	int		rspace;
+	va_list		ap;
+	char		*fmt;
+	char		*str;
+	int			minus;
+	int			zero;
+	int			width;
+	int			prec;
+	int			size;
+	int			dot;
+	int			res;
+	int			nbr;
+	int			lspace;
+	int			rspace;
 }				t_tag;
 
-int		ft_printf(const char *format, ...);
-int		ft_putchar(char c);
-int		ft_putstr(char *s);
-int		ft_isdigit(char c);
-int		strchr_spec(char c);
-char	*ft_itoa_sign(t_tag *tag);
-void	digits_size(t_tag *tag);
-void	sort_width_prec(t_tag *tag);
-void	init_tag(t_tag *tag);
-void	check_tag(t_tag *tag);
-void	print_di(t_tag *tag);
+int				ft_printf(const char *format, ...);
+int				ft_putchar(char c);
+int				ft_putstr(char *s);
+int				ft_isdigit(char c);
+int				strchr_spec(char c);
+char			*ft_itoa_sign(t_tag *tag);
+void			digits_size(t_tag *tag);
+void			sort_width_prec(t_tag *tag);
+void			init_tag(t_tag *tag);
+void			check_tag(t_tag *tag);
+void			print_di(t_tag *tag);
 
 #endif
